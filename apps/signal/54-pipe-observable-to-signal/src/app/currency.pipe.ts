9fb6d22cc9d236ccf74bc3164b28855040321c93
Dakota Methvin <methvind@users.noreply.github.com>
2025-03-09 14:59:51 -0500

    fix(challenge54): refactor to remove unnecessary service
    A service would make sense if we were transforming
    multiple different currencies to one single
    currency or if we otherwise needed to track
    the "current" currency. Since all this example
    is doing is looking up a symbol for a given
    code, we don't need the service.

--- apps/signal/54-pipe-observable-to-signal/src/app/currency.pipe.ts
+++ apps/signal/54-pipe-observable-to-signal/src/app/currency.pipe.ts
@@ -1,18 +1,27 @@
-import { inject, Pipe, PipeTransform } from '@angular/core';
-import { CurrencyService } from './currency.service';
+import { Pipe, PipeTransform } from '@angular/core';
+import { CurrencyCode } from './product.model';
+
+export interface Currency {
+  name: string;
+  code: CurrencyCode;
+  symbol: string;
+}
+
+const currency: Currency[] = [
+  { name: 'Euro', code: 'EUR', symbol: '€' },
+  { name: 'Dollar US', code: 'USD', symbol: 'US$' },
+  { name: 'Dollar Australian', code: 'AUD', symbol: 'AU$' },
+  { name: 'Pound Sterling', code: 'GBP', symbol: '£' },
+  { name: 'Dollar Canadian', code: 'CAD', symbol: 'CAD' },
+];
 
 @Pipe({
   name: 'currency',
   standalone: true,
 })
 export class CurrencyPipe implements PipeTransform {
-  private readonly currencyService = inject(CurrencyService);
-  private readonly symbol = this.currencyService.symbol;
-
-  transform(price: number): string {
-    // TODO: This is not pure because symbol() changes the
-    // output for a given input price. We should change the
-    // pipe to take the code or symbol as a parameter.
-    return `${price}${this.symbol()}`;
+  transform(price: number, code: CurrencyCode): string {
+    let symbol = currency.find((c) => c.code === code)?.symbol ?? code;
+    return `${price}${symbol}`;
   }
 }
