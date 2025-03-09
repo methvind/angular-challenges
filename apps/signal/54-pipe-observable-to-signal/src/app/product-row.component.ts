935157de550852bb93f4e552d65ab5fe2d32af22
Dakota Methvin <methvind@users.noreply.github.com>
2025-03-09 14:59:51 -0500

    fix(challenge54): refactor to remove unnecessary service
    A service would make sense if we were transforming
    multiple different currencies to one single
    currency or if we otherwise needed to track
    the "current" currency. Since all this example
    is doing is looking up a symbol for a given
    code, we don't need the service.

--- apps/signal/54-pipe-observable-to-signal/src/app/product-row.component.ts
+++ apps/signal/54-pipe-observable-to-signal/src/app/product-row.component.ts
@@ -1,32 +1,22 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  Input,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CurrencyPipe } from './currency.pipe';
-import { CurrencyService } from './currency.service';
 import { Product } from './product.model';
 
 @Component({
   selector: 'tr[product-row]',
   template: `
     <td>{{ productInfo.name }}</td>
-    <td>{{ productInfo.priceA | currency }}</td>
-    <td>{{ productInfo.priceB | currency }}</td>
-    <td>{{ productInfo.priceC | currency }}</td>
+    <td>{{ productInfo.priceA | currency: productInfo.currencyCode }}</td>
+    <td>{{ productInfo.priceB | currency: productInfo.currencyCode }}</td>
+    <td>{{ productInfo.priceC | currency: productInfo.currencyCode }}</td>
   `,
   imports: [CurrencyPipe],
-  providers: [CurrencyService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductRowComponent {
   protected productInfo!: Product;
 
   @Input({ required: true }) set product(product: Product) {
-    this.currencyService.updateCode(product.currencyCode);
     this.productInfo = product;
   }
-
-  currencyService = inject(CurrencyService);
 }
