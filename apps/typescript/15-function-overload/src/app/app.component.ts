3f498316f8f74bb364416da75f65022e747d1c97
Dakota Methvin <methvind@users.noreply.github.com>
2025-03-23 13:54:25 -0500

    feat(challenge15): add function overloads to introduce strong typing

--- apps/typescript/15-function-overload/src/app/app.component.ts
+++ apps/typescript/15-function-overload/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { createVehicle } from './vehicle.utils';
 export class AppComponent {
   car = createVehicle('car', 'diesel');
   moto = createVehicle('moto', 'diesel');
-  bus = createVehicle('bus', undefined, 20);
-  boat = createVehicle('boat', undefined, 300, true);
+  bus = createVehicle('bus', 20, true); // Could also have been boat = createVehicle('boat', 20);
+  boat = createVehicle('boat', 300); // Could also have been bus = createVehicle('bus', 300, true);
   bicycle = createVehicle('bicycle');
 }
