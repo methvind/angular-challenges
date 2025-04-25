e12a72a48ac434f6b4c3d8041235fe0db84d0d97
Dakota Methvin <methvind@users.noreply.github.com>
2025-04-24 20:54:22 -0500

    feat(challenge3): use control flow instead of structural directives

--- apps/angular/3-directive-enhancement/src/app/app.component.ts
+++ apps/angular/3-directive-enhancement/src/app/app.component.ts
@@ -1,24 +1,23 @@
-import { NgFor, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 interface Person {
   name: string;
 }
 
+const DEFAULT_PERSONS: Person[] = [{ name: 'Lucy' }, { name: 'Rusty' }];
+
 @Component({
-  imports: [NgFor, NgIf],
   selector: 'app-root',
   template: `
-    <ng-container *ngIf="persons.length > 0; else emptyList">
-      <div *ngFor="let person of persons">
-        {{ person.name }}
-      </div>
-    </ng-container>
-    <ng-template #emptyList>The list is empty !!</ng-template>
+    @for (person of persons; track person.name) {
+      <div>{{ person.name }} ({{ $index + 1 }} / {{ $count }})</div>
+    } @empty {
+      <div>The list is empty !!</div>
+    }
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  persons: Person[] = [];
+  persons: Person[] = DEFAULT_PERSONS;
 }
